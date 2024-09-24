b89c49075eb613fe7a216314dc9160fe5fe0c920
Dani-GT <dani_spain87@hotmail.com>
2024-09-25 00:52:06 +0200

    .

--- ejercicio1.js
+++ ejercicio1.js
@@ -3,12 +3,26 @@
 // no hace falta hacer destructuring del array, solo del objeto.
 
 const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
-
+  
+const { title, gender, year } = game;
+  
+console.log(title); 
+console.log(gender); 
+console.log(year);   
+  
 // 1.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
 // llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
 // imprimelo por consola.
 
 const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+const [fruit1, fruit2, fruit3]  = fruits;
+
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
+
+
 
 // 1.3 En base al siguiente javascript, usa destructuring para crear 2 
 // variables igualandolo a la función e imprimiendolo por consola.
